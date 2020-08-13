cf874c6d2ef3f52c24fcd8b6837bb95f5d07627a
Marcio Goda <marciogoda@gmail.com>
2020-08-13 18:18:22 +0100

    Fixed version comparison

--- publish.sh
+++ publish.sh
@@ -2,24 +2,28 @@
 
 get_terraform_version()
 {
-   terraform_version= wget -q https://registry.hub.docker.com/v1/repositories/hashicorp/terraform/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}' | sort --version-sort -t . | sed -ne 's/[^0-9]*\(\([0-9]*\.\)\{0,4\}[0-9][^.]\)/\1/p' | tail -n 1
+    TERRAFORM_LATEST= wget -q https://registry.hub.docker.com/v1/repositories/hashicorp/terraform/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}' | sort --version-sort -t . | sed -ne 's/[^0-9]*\(\([0-9]*\.\)\{0,4\}[0-9][^.]\)/\1/p' | tail -n 1
 }
 
 get_mergermarket_version()
 {
-    mergermaket_version= wget -q https://registry.hub.docker.com/v1/repositories/mergermarket/terraform/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}' | sort --version-sort -t . | sed -ne 's/[^0-9]*\(\([0-9]*\.\)\{0,4\}[0-9][^.]\)/\1/p' | tail -n 1  
+    MERGERMARKET_LATEST= wget -q https://registry.hub.docker.com/v1/repositories/mergermarket/terraform/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}' | sort --version-sort -t . | sed -ne 's/[^0-9]*\(\([0-9]*\.\)\{0,4\}[0-9][^.]\)/\1/p' | tail -n 1
 }
+
+BUILD_LATEST= false
 
 if [[ "$1" != "" ]]; then
     TERRAFORM_VERSION="$1"
-    BUILD_LATEST=false
 else
-    terraform_version="$(get_terraform_version)"
-    mergermaket_version="$(get_mergermarket_version)"
+    TERRAFORM_LATEST="$(get_terraform_version)"
+    MERGERMARKET_LATEST="$(get_mergermarket_version)"
 
-    if [[ "$terraform_version" != "$mergermarket_version" ]]; then
-        TERRAFORM_VERSION="$terraform_version"
-        BUILD_LATEST=true
+    echo "Terraform Version: $TERRAFORM_LATEST"
+    echo "Mergermarket Version: $MERGERMARKET_LATEST"
+
+    if [ $TERRAFORM_LATEST != $MERGERMARKET_LATEST ]; then
+        TERRAFORM_VERSION="$TERRAFORM_LATEST"
+        BUILD_LATEST= true
 
     else
         echo "Mergermarket version is up to date, nothing to do here!"
@@ -31,7 +35,7 @@ TERRAFORM_IMAGE="hashicorp/terraform:$TERRAFORM_VERSION"
 MERGERMARKET_IMAGE="mergermarket/terraform:$TERRAFORM_VERSION"
 
 docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
-if $BUILD_LATEST; then
+if $BUILD_LATEST ; then
     echo 'Building latest terraform version'
     docker build --build-arg terraform_image_name=${TERRAFORM_IMAGE} -t $MERGERMARKET_IMAGE -t mergermarket/terraform:latest .
     docker push $MERGERMARKET_IMAGE
